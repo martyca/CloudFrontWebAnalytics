0861ad70ad81eccbd9a55e66327aff2c1545f9da
Martin Caarels <martin.caarels@leaven.co.nz>
2024-03-21 19:21:09 +1300

    added gzip

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ docker run -it --rm \
     -e AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY \
     -e AWS_SESSION_TOKEN martyca:goaccess bash
+
+    #gunzip -c *.gz | goaccess        --log-format "%d\\t%t\\t%^\\t%b\\t%h\\t%m\\t%^\\t%r\\t%s\\t%R\\t%u\\t%^"        --date-format CLOUDFRONT   --time-format CLOUDFRONT        --ignore-crawlers       --ignore-status=301     --ignore-status=302 -a > report.html
